cd50c96f6a3a08d4259b527f38b60d6c28e23b77
itsthecipriano <itsthecipriano@users.noreply.github.com>
2021-02-16 12:19:43 +0300

    Add project files.

---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Product:IEntity
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProcuctName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public short UnitsInStock { get; set; }
+    }
+}
